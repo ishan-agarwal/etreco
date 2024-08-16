ae122da4a1ba7adc4089fb1e71088b3b637e9e27
Ishan <ishanagarwal1605@gmail.com>
2024-08-16 23:55:25 +0530

    added recommender and date column

--- etreco_app/recommend.py
+++ etreco_app/recommend.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 import re
 import json
 import requests
@@ -14,46 +15,14 @@ from record import Record
 
 time.sleep(20)  # wait for mysql container to be up
 RECORDER = Record()
+
 SCRAPE_URL = "https://economictimes.indiatimes.com/markets/stocks/recos"
+LAST_CLOSE_PRICE_URL = "http://192.168.0.125:5000/last_close_price"
+
 NSE_DICT = pd.read_csv("etc/nse_list.csv").set_index("Symbol")["Company Name"].to_dict()
 with open("etc/bse_list.json", "r") as file:
     BSE_DICT = json.load(file)
 ALL_DICT = NSE_DICT | BSE_DICT
-
-
-
-def get_last_close_price(symbol):
-    url = f"http://192.168.0.125:5000/last_close_price?ticker={symbol}"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
-        data = response.json()
-        return round(data.get(symbol, 0), 2)
-    except (requests.exceptions.RequestException, ValueError, KeyError):
-        return 0
-
-
-def scrape():
-    url = SCRAPE_URL
-    pattern = re.compile(r"((Buy)|(Sell)) (.*), target price Rs (\d*)(\.)*:(.*)")
-    response = requests.get(url)
-    recommendations = {}  # Key=Stock Name, Value=Target Price
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        reco_sections = soup.find_all("div", class_="eachStory")
-        for section in reco_sections:
-            try:
-                a_text = section.find("a").text.strip()
-                match = pattern.match(a_text)
-                if match.group(1) == "Buy":
-                    # print(match.group(1), " ", match.group(4), "; TP : ", match.group(5))
-                    recommendations[match.group(4)] = int(match.group(5))
-            except AttributeError:
-                print("AttributeError")
-                continue
-    else:
-        print("Failed to retrieve the webpage")
-    return recommendations
 
 
 def get_symbol_of(company_name):
@@ -74,27 +43,65 @@ def get_symbol_of(company_name):
             return symbol
 
 
-def get_pct_change(recs):
-    for company_name, tp in recs.items():
-        symbol = get_symbol_of(company_name=company_name)
-        if symbol == -1:
-            continue
-        symbol = symbol + ".NS" if symbol.isalpha() else symbol + ".BO"
-        print("getting ltp for ", company_name)
-        ltp = get_last_close_price(symbol)
-        RECORDER.add_row(
-           symbol , company_name, TP=tp, LTP=ltp
-        )
+def get_last_close_price(symbol):
+    try:
+        params = {"ticker": symbol}
+        response = requests.get(LAST_CLOSE_PRICE_URL, params=params)
+        response.raise_for_status()
+        data = response.json()
+        return round(data.get(symbol, 0), 2)
+    except (requests.exceptions.RequestException, ValueError, KeyError):
+        return 0
 
 
-def collect_recos():
-    recs = scrape()
-    get_pct_change(recs=recs)
-    # RECORDER.close_conn()
+def get_date(date_str):
+    try:
+        date_obj = datetime.strptime(date_str, "%b %d, %Y, %I:%M %p %Z")
+    except ValueError:
+        date_obj = datetime.strptime(date_str, "%b %d, %Y")
+    return date_obj.date()
 
 
-schedule.every().day.at("07:00").do(collect_recos)
+def main():
+    url = SCRAPE_URL
+    pattern = re.compile(r"((Buy)|(Sell)) (.*), target price Rs (\d*)(\.)*:(.*)")
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        reco_sections = soup.find_all("div", class_="eachStory")
+        for section in reco_sections:
+            try:
+                a_text = section.find("a").text.strip()
+                match = pattern.match(a_text)
+                side = match.group(1)
+                company_name = match.group(4)
+                target_price = match.group(5)
+                recommender = match.group(7)
+                time_text = section.find("time").text.strip()
+                if side != "Buy":
+                    continue
+                symbol = get_symbol_of(company_name)
+                if symbol == -1:
+                    continue
+                else:
+                    symbol = symbol + ".NS" if symbol.isalpha() else symbol + ".BO"
+                ltp = get_last_close_price(symbol)
+                RECORDER.add_row(
+                    symbol,
+                    company_name,
+                    recommender.strip(),
+                    get_date(time_text),
+                    TP=target_price,
+                    LTP=ltp,
+                )
+            except:
+                print("Error in Main Function")
+                continue
+    else:
+        print("Failed to retrieve the webpage")
+    return
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+
+main()
+
+RECORDER.close_conn()
--- etreco_app/record.py
+++ etreco_app/record.py
@@ -12,20 +12,17 @@ class Record:
             "port": os.environ.get("DB_PORT"),
             "database": os.environ.get("DB_NAME"),
         }
-        self.columns = [
-            "Symbol",
-            "CompanyName",
-            "TP",
-            "LTP",
-        ]
         self.conn = mysql.connector.connect(**self.db_config)
         self.cursor = self.conn.cursor()
         self.create_table()
+
     def create_table(self):
         create_table_query = """
-        CREATE TABLE IF NOT EXISTS records (
+        CREATE TABLE IF NOT EXISTS recos (
             Symbol VARCHAR(255),
             CompanyName VARCHAR(255),
+            Recommender VARCHAR(255),
+            DateOfRecommendation DATE,
             TP FLOAT,
             LTP FLOAT,
             PRIMARY KEY (Symbol)
@@ -33,18 +30,22 @@ class Record:
         """
         self.cursor.execute(create_table_query)
         self.conn.commit()
-    def add_row(self, Symbol, CompanyName, TP, LTP):
+
+    def add_row(self, Symbol, CompanyName, Recommender, Date, TP, LTP):
         insert_query = """
-            INSERT INTO records (Symbol, CompanyName, TP, LTP)
-            VALUES (%s, %s, %s, %s)
+            INSERT INTO recos (Symbol, CompanyName, Recommender, DateOfRecommendation, TP, LTP)
+            VALUES (%s, %s, %s, %s, %s, %s)
             ON DUPLICATE KEY UPDATE
             LTP = VALUES(LTP)
             """
         try:
-            self.cursor.execute(insert_query, (Symbol, CompanyName, TP, LTP))
+            self.cursor.execute(
+                insert_query, (Symbol, CompanyName, Recommender, Date, TP, LTP)
+            )
             self.conn.commit()
         except mysql.connector.Error as err:
             print(f"Error: {err}")
+
     def close_conn(self):
         self.cursor.close()
         self.conn.close()
